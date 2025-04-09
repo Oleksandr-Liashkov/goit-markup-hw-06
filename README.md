e844f13d57e71b2917e3475677317c7a23663456
Oleksandr Liashkov <aleksandr.liashkov98@gmail.com>
2025-04-09 17:47:18 +0300

    v 0.5.1

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ v(version) 0(page).0(section).0(update)
 - v 0.4.0 - Modal completed.
 
 - v 0.5.0 - Mobile Menu completed.
+
+- v 0.5.1 - Small updates.
--- js/modal.js
+++ js/modal.js
@@ -5,12 +5,12 @@
     // Додати атрибут data-modal-close на кнопку закриття
     closeModalBtn: document.querySelector("[data-modal-close]"),
     // Додати атрибут data-modal на бекдроп модалки
-    modal: document.querySelector("[data-menu]"),
+    modal: document.querySelector("[data-modal]"),
   };
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
   function toggleModal() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.modal.classList.toggle("is-open");
+    refs.modal.classList.toggle("is-open-modal");
   }
 })();

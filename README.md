68371a29caa73ce55cce217b7bcee9db3e3dfd67
Oleksandr Liashkov <aleksandr.liashkov98@gmail.com>
2025-04-09 15:17:19 +0300

    v 0.5.0

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ v(version) 0(page).0(section).0(update)
 - v 0.3.0 - Footer completed.
 
 - v 0.4.0 - Modal completed.
+
+- v 0.5.0 - Mobile Menu completed.
--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,9 @@
 (() => {
   const refs = {
     // Додати атрибут data-modal-open на кнопку відкриття
-    openModalBtn: document.querySelector("[data-modal-open]"),
+    openModalBtn: document.querySelector("[data-menu-open]"),
     // Додати атрибут data-modal-close на кнопку закриття
-    closeModalBtn: document.querySelector("[data-modal-close]"),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
     // Додати атрибут data-modal на бекдроп модалки
     modal: document.querySelector("[data-menu]"),
   };
